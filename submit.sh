1af6dd4b0a847419a7d30d594591181ce761b266
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-03-17 00:12:27 -0300

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -50,6 +50,9 @@ certbot certonly --non-interactive --agree-tos --register-unsafely-without-email
 
 echo "==================================================================== Hostname && SSL ===================================================================="
 
+sudo hostname $ServerName
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apache2 php php-cli php-dev php-curl php-gd libapache2-mod-php --assume-yes
+
 echo "==================================================================== DKIM ==============================================================================="
 
 # Instalar pacotes e configurar serviços de e-mail
