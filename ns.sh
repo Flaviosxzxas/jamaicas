4c535891ba82811a58bc68a6f3c79b85be81b5ea
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-09-19 11:42:05 -0300

    Update ns.sh

--- ns.sh
+++ ns.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 fi
 
 ServerName=$1
-
+echo "==================================================== APPLICATION ===================================================="
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo hostnamectl set-hostname "$ServerName"
@@ -23,3 +23,13 @@ echo "postfix postfix/main_mailer_type string 'Internet Site'" | sudo debconf-se
 sudo apt-get install -y nano apache2 php7.4 libapache2-mod-php7.4 php7.4-cli php7.4-mysql php7.4-gd php7.4-imagick php7.4-tidy php7.4-xmlrpc php7.4-common php7.4-xml php7.4-curl php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl toilet unzip curl postfix --allow-unauthenticated --assume-yes
 echo 'BlackRock /2024' | sudo toilet --filter metal > /etc/motd
 sudo ufw disable
+echo "==================================================== APPLICATION ===================================================="
+
+echo "================================= Todos os comandos foram executados com sucesso! ==================================="
+
+echo "======================================================= FIM =========================================================="
+
+# Reiniciar servidor
+echo "Reiniciando o servidor em 5 segundos..."
+sleep 5
+sudo reboot
