cef9162856f8429f00661b73072e904f00fd0127
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-12-08 13:04:06 -0300

    Update buces.sh

--- buces.sh
+++ buces.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if ! dpkg -l | grep -q postfix-policyd-spf-python; then
+    echo "Pacote postfix-policyd-spf-python não encontrado. Instalando..."
+    sudo apt update
+    sudo apt install postfix-policyd-spf-python -y
+else
+    echo "Pacote postfix-policyd-spf-python já está instalado."
+fi
+
+# Adiciona regras do Policyd
+echo "#######################################################
+
 sudo systemctl restart postfix-policyd-spf
 if [ $? -eq 0 ]; then
     echo "Serviço reiniciado com sucesso."
