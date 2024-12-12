0e33e866ce185f7dc5d2f038edf297497877f77b
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-12-12 13:10:37 -0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -43,68 +43,54 @@ if [ -z "$CloudflareZoneID" ]; then
 fi
 
 # Função para verificar a existência de um registro
-record_exists() {
+get_record_details() {
   local record_name=$1
   local record_type=$2
   curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records?name=$record_name&type=$record_type" \
     -H "X-Auth-Email: $CloudflareEmail" \
     -H "X-Auth-Key: $CloudflareAPI" \
-    -H "Content-Type: application/json" | jq -r '.result | length'
+    -H "Content-Type: application/json"
 }
 
-# Criar registro A
-if [ $(record_exists "$DKIMSelector" "A") -eq 0 ]; then
-  echo "  -- Cadastrando A"
-  response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
-       -H "X-Auth-Email: $CloudflareEmail" \
-       -H "X-Auth-Key: $CloudflareAPI" \
-       -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "A" --arg name "$DKIMSelector" --arg content "$ServerIP" --argjson ttl 120 --argjson proxied false \
-          '{type: $type, name: $name, content: $content, ttl: $ttl, proxied: $proxied}')")
-  echo "Response (A): $response" >> /root/cloudflare_logs.txt
-else
-  echo "Registro A já existe. Pulando." >> /root/cloudflare_logs.txt
-fi
+record_exists() {
+  local record_name=$1
+  local record_type=$2
+  get_record_details "$record_name" "$record_type" | jq -r '.result | length'
+}
 
-# Criar registro SPF
-if [ $(record_exists "$ServerName" "TXT") -eq 0 ]; then
-  echo "  -- Cadastrando SPF"
-  response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
-       -H "X-Auth-Email: $CloudflareEmail" \
-       -H "X-Auth-Key: $CloudflareAPI" \
-       -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "$ServerName" --arg content "\"v=spf1 a:$ServerName ~all\"" --arg ttl "120" --argjson proxied false \
-          '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
-  echo "Response (SPF): $response" >> /root/cloudflare_logs.txt
-else
-  echo "Registro SPF já existe. Pulando." >> /root/cloudflare_logs.txt
-fi
+create_or_update_record() {
+  local record_name=$1
+  local record_type=$2
+  local record_content=$3
+  local record_ttl=120
+  local record_priority=$4
+  local record_proxied=false
 
-# Criar registro DMARC
-if [ $(record_exists "_dmarc.$ServerName" "TXT") -eq 0 ]; then
-  echo "  -- Cadastrando DMARC"
-  response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
-       -H "X-Auth-Email: $CloudflareEmail" \
-       -H "X-Auth-Key: $CloudflareAPI" \
-       -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "_dmarc.$ServerName" --arg content "\"v=DMARC1; p=quarantine; sp=quarantine; rua=mailto:dmarc@$ServerName; rf=afrf; fo=0:1:d:s; ri=86000; adkim=r; aspf=r\"" --arg ttl "120" --argjson proxied false \
-          '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
-  echo "Response (DMARC): $response" >> /root/cloudflare_logs.txt
-else
-  echo "Registro DMARC já existe. Pulando." >> /root/cloudflare_logs.txt
-fi
+  response=$(get_record_details "$record_name" "$record_type")
+  existing_content=$(echo "$response" | jq -r '.result[0].content')
+  existing_ttl=$(echo "$response" | jq -r '.result[0].ttl')
+  existing_priority=$(echo "$response" | jq -r '.result[0].priority')
 
-# Criar registro DKIM
-if [ $(record_exists "mail._domainkey.$ServerName" "TXT") -eq 0 ]; then
-  echo "  -- Cadastrando DKIM"
-  EscapedDKIMCode=$(printf '%s' "$DKIMCode" | sed 's/\"/\\\\\"/g')
-  response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
-       -H "X-Auth-Email: $CloudflareEmail" \
-       -H "X-Auth-Key: $CloudflareAPI" \
-       -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "mail._domainkey.$ServerName" --arg content "\"v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode\"" --arg ttl "120" --argjson proxied false \
-          '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
-  echo "Response (DKIM): $response" >> /root/cloudflare_logs.txt
-else
-  echo "Registro DKIM já existe. Pulando." >> /root/cloudflare_logs.txt
-fi
+  if [ "$record_type" == "MX" ] && [ "$existing_content" == "$record_content" ] && [ "$existing_ttl" -eq "$record_ttl" ] && [ "$existing_priority" -eq "$record_priority" ]; then
+    echo "Registro $record_type para $record_name já está atualizado. Pulando."
+  elif [ "$record_type" != "MX" ] && [ "$existing_content" == "$record_content" ] && [ "$existing_ttl" -eq "$record_ttl" ]; then
+    echo "Registro $record_type para $record_name já está atualizado. Pulando."
+  else
+    echo "  -- Criando ou atualizando registro $record_type para $record_name"
+    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
+         -H "X-Auth-Email: $CloudflareEmail" \
+         -H "X-Auth-Key: $CloudflareAPI" \
+         -H "Content-Type: application/json" \
+         --data "$(jq -n --arg type "$record_type" --arg name "$record_name" --arg content "$record_content" --arg ttl "$record_ttl" --argjson proxied "$record_proxied" --arg priority "$record_priority" \
+            '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied, priority: ($priority | tonumber)}')"
+  fi
+}
+
+# Criar ou atualizar registros
+echo "  -- Configurando registros DNS"
+create_or_update_record "$DKIMSelector" "A" "$ServerIP" ""
+create_or_update_record "$ServerName" "TXT" "\"v=spf1 a:$ServerName ~all\"" ""
+create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=quarantine; sp=quarantine; rua=mailto:dmarc@$ServerName; rf=afrf; fo=0:1:d:s; ri=86000; adkim=r; aspf=r\"" ""
+EscapedDKIMCode=$(printf '%s' "$DKIMCode" | sed 's/\"/\\\"/g')
+create_or_update_record "mail._domainkey.$ServerName" "TXT" "\"v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode\"" ""
+create_or_update_record "$ServerName" "MX" "$ServerName" "10"
