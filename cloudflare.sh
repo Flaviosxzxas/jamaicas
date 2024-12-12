eeae8fb726f0654fa77f48de0933e18b5d4e7042
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-12-12 12:38:47 -0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,3 +1,71 @@
+#!/bin/bash
+
+# Verifique se o script está sendo executado como root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Este script precisa ser executado como root."
+  exit 1
+fi
+
+# Atualizar a lista de pacotes e atualizar pacotes
+echo "Atualizando a lista de pacotes..."
+sudo apt-get update
+sudo apt-get upgrade -y
+
+ServerName=$1
+CloudflareAPI=$2
+CloudflareEmail=$3
+
+Domain=$(echo $ServerName | cut -d "." -f2-)
+DKIMSelector=$(echo $ServerName | awk -F[.:] '{print $1}')
+ServerIP=$(wget -qO- http://ip-api.com/line\?fields=query)
+
+echo "Configurando Servidor: $ServerName"
+echo "Domain: $Domain"
+echo "DKIMSelector: $DKIMSelector"
+echo "ServerIP: $ServerIP"
+
+sleep 5
+
+echo "==================================================== CLOUDFLARE ===================================================="
+
+DKIMCode=$(/root/dkimcode.sh)
+
+# Obter o ID da zona do Cloudflare
+echo "  -- Obtendo Zona"
+CloudflareZoneID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$Domain&status=active" \
+  -H "X-Auth-Email: $CloudflareEmail" \
+  -H "X-Auth-Key: $CloudflareAPI" \
+  -H "Content-Type: application/json" | jq -r '.result[0].id')
+
+if [ -z "$CloudflareZoneID" ]; then
+  echo "Erro: Não foi possível obter o ID da zona do Cloudflare." >&2
+  exit 1
+fi
+
+# Função para verificar a existência de um registro
+record_exists() {
+  local record_name=$1
+  local record_type=$2
+  curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records?name=$record_name&type=$record_type" \
+    -H "X-Auth-Email: $CloudflareEmail" \
+    -H "X-Auth-Key: $CloudflareAPI" \
+    -H "Content-Type: application/json" | jq -r '.result | length'
+}
+
+# Criar registro A
+if [ $(record_exists "$DKIMSelector" "A") -eq 0 ]; then
+  echo "  -- Cadastrando A"
+  response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$CloudflareZoneID/dns_records" \
+       -H "X-Auth-Email: $CloudflareEmail" \
+       -H "X-Auth-Key: $CloudflareAPI" \
+       -H "Content-Type: application/json" \
+       --data "$(jq -n --arg type "A" --arg name "$DKIMSelector" --arg content "$ServerIP" --argjson ttl 120 --argjson proxied false \
+          '{type: $type, name: $name, content: $content, ttl: $ttl, proxied: $proxied}')")
+  echo "Response (A): $response" >> /root/cloudflare_logs.txt
+else
+  echo "Registro A já existe. Pulando." >> /root/cloudflare_logs.txt
+fi
+
 # Criar registro SPF
 if [ $(record_exists "$ServerName" "TXT") -eq 0 ]; then
   echo "  -- Cadastrando SPF"
