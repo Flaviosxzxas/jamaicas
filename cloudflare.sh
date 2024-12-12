fc1e40f9435631836876b4d5baa70f6de4ca55fc
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-12-12 12:44:14 -0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -73,7 +73,7 @@ if [ $(record_exists "$ServerName" "TXT") -eq 0 ]; then
        -H "X-Auth-Email: $CloudflareEmail" \
        -H "X-Auth-Key: $CloudflareAPI" \
        -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "$ServerName" --arg content "v=spf1 a:$ServerName ~all" --arg ttl "120" --argjson proxied false \
+       --data "$(jq -n --arg type "TXT" --arg name "$ServerName" --arg content "\"v=spf1 a:$ServerName ~all\"" --arg ttl "120" --argjson proxied false \
           '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
   echo "Response (SPF): $response" >> /root/cloudflare_logs.txt
 else
@@ -87,7 +87,7 @@ if [ $(record_exists "_dmarc.$ServerName" "TXT") -eq 0 ]; then
        -H "X-Auth-Email: $CloudflareEmail" \
        -H "X-Auth-Key: $CloudflareAPI" \
        -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "_dmarc.$ServerName" --arg content "v=DMARC1; p=quarantine; sp=quarantine; rua=mailto:dmarc@$ServerName; rf=afrf; fo=0:1:d:s; ri=86000; adkim=r; aspf=r" --arg ttl "120" --argjson proxied false \
+       --data "$(jq -n --arg type "TXT" --arg name "_dmarc.$ServerName" --arg content "\"v=DMARC1; p=quarantine; sp=quarantine; rua=mailto:dmarc@$ServerName; rf=afrf; fo=0:1:d:s; ri=86000; adkim=r; aspf=r\"" --arg ttl "120" --argjson proxied false \
           '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
   echo "Response (DMARC): $response" >> /root/cloudflare_logs.txt
 else
@@ -102,10 +102,9 @@ if [ $(record_exists "mail._domainkey.$ServerName" "TXT") -eq 0 ]; then
        -H "X-Auth-Email: $CloudflareEmail" \
        -H "X-Auth-Key: $CloudflareAPI" \
        -H "Content-Type: application/json" \
-       --data "$(jq -n --arg type "TXT" --arg name "mail._domainkey.$ServerName" --arg content "v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode" --arg ttl "120" --argjson proxied false \
+       --data "$(jq -n --arg type "TXT" --arg name "mail._domainkey.$ServerName" --arg content "\"v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode\"" --arg ttl "120" --argjson proxied false \
           '{type: $type, name: $name, content: $content, ttl: ($ttl | tonumber), proxied: $proxied}')")
   echo "Response (DKIM): $response" >> /root/cloudflare_logs.txt
 else
   echo "Registro DKIM já existe. Pulando." >> /root/cloudflare_logs.txt
 fi
-
