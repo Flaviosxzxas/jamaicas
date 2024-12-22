0fd45247402c12e63d41733b255a1acfc643cdad
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2024-12-22 10:17:47 -0300

    Update buces.sh

--- buces.sh
+++ buces.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # Verifique se o script está sendo executado como root
 if [ "$(id -u)" -ne 0 ]; then
   echo "Este script precisa ser executado como root."
@@ -43,20 +41,11 @@ Restart=always
 RestartSec=5
 User=root
 Group=root
-StandardOutput=syslog
-StandardError=syslog
 SyslogIdentifier=postfix-policyd-spf-python
 
 [Install]
 WantedBy=multi-user.target
 EOF'
-
-# Remover o serviço postfix-policyd-spf-python (caso necessário)
-echo "Removendo o serviço postfix-policyd-spf-python para evitar reinicializações desnecessárias..."
-sudo systemctl stop postfix-policyd-spf-python
-sudo systemctl disable postfix-policyd-spf-python
-sudo rm /etc/systemd/system/postfix-policyd-spf-python.service
-sudo systemctl daemon-reload
 
 # Consolidar reinicializações ao final do script
 echo "Recarregando e reiniciando os serviços..."
