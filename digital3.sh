cc251b4b390ec9787c0e155cb1c1e246a83cc82d
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-01-03 17:46:24 -0300

    Update digital3.sh

--- digital3.sh
+++ digital3.sh
@@ -537,118 +537,118 @@ pidfile=/run/postfwd/postfwd.pid
 # KingHost
 id=limit-kinghost
 pattern=recipient mx=.*kinghost.net
-action='rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para KingHost."'
+action=rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para KingHost."
 
 # UOL Host
 id=limit-uolhost
 pattern=recipient mx=.*uhserver
-action='rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para UOL Host."'
+action=rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para UOL Host."
 
 # LocaWeb
 id=limit-locaweb
 pattern=recipient mx=.*locaweb.com.br
-action='rate(global/500/3600) defer_if_permit "Limite de 500 e-mails por hora atingido para LocaWeb."'
+action=rate(global/500/3600) defer_if_permit "Limite de 500 e-mails por hora atingido para LocaWeb."
 
 # Yahoo (Contas Pessoais)
 id=limit-yahoo
 pattern=recipient mx=.*yahoo.com
-action='rate(global/150/3600) defer_if_permit "Limite de 150 e-mails por hora atingido para Yahoo."'
+action=rate(global/150/3600) defer_if_permit "Limite de 150 e-mails por hora atingido para Yahoo."
 
 # Mandic
 id=limit-mandic
 pattern=recipient mx=.*mandic.com.br
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Mandic."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Mandic."
 
 # Titan
 id=limit-titan
 pattern=recipient mx=.*titan.email
-action='rate(global/500/3600) defer_if_permit "Limite de 500 e-mails por hora atingido para Titan."'
+action=rate(global/500/3600) defer_if_permit "Limite de 500 e-mails por hora atingido para Titan."
 
 # Google (Contas Pessoais e G Suite)
 id=limit-google
 pattern=recipient mx=.*google
-action='rate(global/2000/3600) defer_if_permit "Limite de 2000 e-mails por hora atingido para Google."'
+action=rate(global/2000/3600) defer_if_permit "Limite de 2000 e-mails por hora atingido para Google."
 
 # Hotmail (Contas Pessoais)
 id=limit-hotmail
 pattern=recipient mx=.*hotmail.com
-action='rate(global/1000/86400) defer_if_permit "Limite de 1000 e-mails por dia atingido para Hotmail."'
+action=rate(global/1000/86400) defer_if_permit "Limite de 1000 e-mails por dia atingido para Hotmail."
 
 # Office 365 (Contas Empresariais)
 id=limit-office365
 pattern=recipient mx=.*outlook.com
-action='rate(global/2000/3600) defer_if_permit "Limite de 2000 e-mails por hora atingido para Office 365."'
+action=rate(global/2000/3600) defer_if_permit "Limite de 2000 e-mails por hora atingido para Office 365."
 
 # Secureserver (GoDaddy)
 id=limit-secureserver
 pattern=recipient mx=.*secureserver.net
-action='rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para GoDaddy."'
+action=rate(global/300/3600) defer_if_permit "Limite de 300 e-mails por hora atingido para GoDaddy."
 
 # Zimbra
 id=limit-zimbra
 pattern=recipient mx=.*zimbra
-action='rate(global/400/3600) defer_if_permit "Limite de 400 e-mails por hora atingido para Zimbra."'
+action=rate(global/400/3600) defer_if_permit "Limite de 400 e-mails por hora atingido para Zimbra."
 
 # Provedores na Argentina
 # Fibertel
 id=limit-fibertel
 pattern=recipient mx=.*fibertel.com.ar
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Fibertel."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Fibertel."
 
 # Speedy
 id=limit-speedy
 pattern=recipient mx=.*speedy.com.ar
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Speedy."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Speedy."
 
 # Personal (Arnet)
 id=limit-personal
 pattern=recipient mx=.*personal.com.ar
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Personal Arnet."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Personal Arnet."
 
 # Telecom
 id=limit-telecom
 pattern=recipient mx=.*telecom.com.ar
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telecom."'
+action=rate(global /200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telecom."
 
 # Claro
 id=limit-claro
 pattern=recipient mx=.*claro.com.ar
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Claro."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Claro."
 
 # Provedores no México
 # Telmex
 id=limit-telmex
 pattern=recipient mx=.*prodigy.net.mx
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telmex."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telmex."
 
 # Axtel
 id=limit-axtel
 pattern=recipient mx=.*axtel.net
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para A xtel."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Axtel."
 
 # Izzi Telecom
 id=limit-izzi
 pattern=recipient mx=.*izzi.net.mx
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Izzi Telecom."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Izzi Telecom."
 
 # Megacable
 id=limit-megacable
 pattern=recipient mx=.*megacable.com.mx
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Megacable."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Megacable."
 
 # TotalPlay
 id=limit-totalplay
 pattern=recipient mx=.*totalplay.net.mx
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para TotalPlay."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para TotalPlay."
 
 # Telcel
 id=limit-telcel
 pattern=recipient mx=.*telcel.net
-action='rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telcel."'
+action=rate(global/200/3600) defer_if_permit "Limite de 200 e-mails por hora atingido para Telcel."
 
 # Outros (Sem Limite)
 id=no-limit
-pattern=recipient
+pattern=recipient mx=.*
 action=permit
 EOF
 fi
