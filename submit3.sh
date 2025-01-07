6e6ad4d89ab8dcb473e939afe1eaeb2535b4617a
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-01-07 15:47:21 -0300

    Update submit3.sh

--- submit3.sh
+++ submit3.sh
@@ -460,15 +460,21 @@ inet_protocols = all" | sudo tee /etc/postfix/main.cf > /dev/null
 POSTFIX_FILES="/etc/postfix/postfix-files"
 MAKDEFS_LINE='$meta_directory/makedefs.out:f:root:-:644'
 
-if grep -q "$MAKDEFS_LINE" "$POSTFIX_FILES"; then
-    echo "Encontrada entrada 'makedefs.out' no arquivo $POSTFIX_FILES. Comentando a linha..."
-    sudo sed -i "s|$MAKDEFS_LINE|#$MAKDEFS_LINE|" "$POSTFIX_FILES" || {
-        echo "Erro ao comentar a linha 'makedefs.out' no arquivo $POSTFIX_FILES."
-        exit 1
-    }
-    echo "Linha 'makedefs.out' comentada com sucesso no arquivo $POSTFIX_FILES."
+# Verificar se o arquivo 'postfix-files' existe
+if [ -f "$POSTFIX_FILES" ]; then
+    # Verificar se a linha específica existe no arquivo
+    if grep -q "$MAKDEFS_LINE" "$POSTFIX_FILES"; then
+        echo "Encontrada entrada 'makedefs.out' no arquivo $POSTFIX_FILES. Comentando a linha..."
+        sudo sed -i "s|$MAKDEFS_LINE|#$MAKDEFS_LINE|" "$POSTFIX_FILES" || {
+            echo "Erro ao comentar a linha 'makedefs.out' no arquivo $POSTFIX_FILES."
+            exit 1
+        }
+        echo "Linha 'makedefs.out' comentada com sucesso no arquivo $POSTFIX_FILES."
+    else
+        echo "A entrada 'makedefs.out' não foi encontrada no arquivo $POSTFIX_FILES. Nenhuma ação necessária."
+    fi
 else
-    echo "A entrada 'makedefs.out' já está comentada ou não foi encontrada no arquivo $POSTFIX_FILES."
+    echo "O arquivo $POSTFIX_FILES não foi encontrado. Pulando a configuração para 'makedefs.out'."
 fi
 
 # Remover o arquivo 'makedefs.out' se existir
@@ -482,6 +488,7 @@ if [ -e "/etc/postfix/makedefs.out" ]; then
 else
     echo "Arquivo /etc/postfix/makedefs.out não encontrado. Nenhuma ação necessária."
 fi
+
 
 # Salvar variáveis antes de instalar dependências
 ORIGINAL_VARS=$(declare -p ServerName CloudflareAPI CloudflareEmail Domain DKIMSelector ServerIP)
