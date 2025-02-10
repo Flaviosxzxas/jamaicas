cd0d30116026f4c3c867187f17c9f68bf6fd7b7b
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-02-10 00:36:53 -0300

    Update submit4.sh

--- submit4.sh
+++ submit4.sh
@@ -68,33 +68,6 @@ echo "======================"
 sleep 10
 
 echo "==================================================================== Hostname && SSL ===================================================================="
-# ============================================
-# Permitir SSH antes de ativar o ufw
-ufw allow OpenSSH
-# ============================================
-# ============================================
-#  Verificar se o ufw está ativo
-# ============================================
-if ! ufw status | grep -q "Status: active"; then
-  echo "UFW não está ativo. Ativando..."
-  ufw enable
-else
-  echo "UFW já está ativo."
-fi
-
-# ============================================
-#  Bloquear todas as conexões externas para a porta 25
-# ============================================
-ufw deny 25/tcp
-
-# ============================================
-#  Permitir tráfego local na porta 25
-# ============================================
-ufw allow from 127.0.0.1 to any port 25 proto tcp
-
-# Recarregar o firewall para aplicar as regras
-ufw reload
-
 
 # ============================================
 #  Instalar pacotes básicos
