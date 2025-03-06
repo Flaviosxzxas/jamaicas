e28b41993a952aca2bcbd5ad4453c6f0212bf70d
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-03-06 10:44:08 -0300

    Update digital4.sh

--- digital4.sh
+++ digital4.sh
@@ -8,9 +8,6 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-# ============================================
-#  Atualizar pacotes
-# ============================================
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Atualizando pacotes..."
