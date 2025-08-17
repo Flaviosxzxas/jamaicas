e3a5163c35da3f757fad30d3135fedb2d0932f89
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-08-16 22:43:06 -0300

    Update binarylane.sh

--- binarylane.sh
+++ binarylane.sh
@@ -151,20 +151,16 @@ npm install -g pm2 && echo "PM2 instalado: versão $(pm2 -v)" || {
 
 mkdir -p /root/.secrets && chmod 0700 /root/.secrets/ && touch /root/.secrets/cloudflare.cfg && chmod 0400 /root/.secrets/cloudflare.cfg
 
-# limpa entradas antigas do FQDN
+# /etc/hosts: manter localhost e apontar FQDN -> IP público
 sed -i "\|[[:space:]]$ServerName\$|d" /etc/hosts
-sed -i "\|127\.0\.1\.1[[:space:]]\+$ServerName\$|d" /etc/hosts
+sed -i "\|^\s*127\.0\.1\.1\s\+$ServerName\$|d" /etc/hosts
 
-# garante linha de localhost IPv4
 grep -qE '^\s*127\.0\.0\.1\s+localhost(\s|$)' /etc/hosts || \
   sed -i '1i 127.0.0.1\tlocalhost' /etc/hosts
 
-# remove entradas antigas do FQDN e adiciona a correta (FQDN -> IP público)
-sed -i "\|[[:space:]]$ServerName\$|d" /etc/hosts
-sed -i "\|127\.0\.1\.1[[:space:]]\+$ServerName\$|d" /etc/hosts
 printf '%s\t%s\n' "$ServerIP" "$ServerName" >> /etc/hosts
 
-# hostname
+# hostname (opcional; hostnamectl já atualiza /etc/hostname)
 hostnamectl set-hostname "$ServerName"
 
 certbot certonly --non-interactive --agree-tos --register-unsafely-without-email \
