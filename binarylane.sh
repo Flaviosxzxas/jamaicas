1605eedc12d87fa248dbdf00eae4a5828b4dd8e9
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2025-08-25 18:55:18 -0300

    Update binarylane.sh

--- binarylane.sh
+++ binarylane.sh
@@ -680,7 +680,6 @@ DKIMCode=$(echo "$DKIMCode" | tr -d '\n' | tr -s ' ')
 EscapedDKIMCode=$(printf '%s' "$DKIMCode" | sed 's/\"/\\\"/g')
 
 create_or_update_record "$DKIMSelector" "A" "$ServerIP" ""
-create_or_update_record "$ServerName" "TXT" "\"v=spf1 a mx ip4:$ServerIP ~all\"" ""
 create_or_update_record "$ServerName" "TXT" "\"v=spf1 a mx ip4:$ServerIP include:$ServerName ~all\"" ""
 create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=none; rua=mailto:dmarc-reports@$ServerName; ruf=mailto:dmarc-reports@$ServerName; sp=none; adkim=s; aspf=s\"" ""
 create_or_update_record "mail._domainkey.$ServerName" "TXT" "\"v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode\"" ""
