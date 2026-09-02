17f97a0713a2ebf3439bc81a105aff73abc517db
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2026-09-01 21:30:15 -0300

    Update buces1.sh

--- buces1.sh
+++ buces1.sh
@@ -1232,13 +1232,13 @@ create_or_update_record "$ServerName" "TXT" "\"v=spf1 ip4:$ServerIP -all\"" ""
 create_or_update_record "$MailServerName" "TXT" "\"v=spf1 ip4:$ServerIP -all\"" ""
 
 # DMARC - domínio novo + envio em massa: fase de monitoramento
-create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=none; sp=none; pct=100; rua=mailto:dmarc-reports@$ServerName; adkim=r; aspf=r; fo=1\"" ""
+# create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=none; sp=none; pct=100; rua=mailto:dmarc-reports@$ServerName; adkim=r; aspf=r; fo=1\"" ""
 
 # DMARC - após alguns dias, se tudo estiver passando
 # create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=quarantine; sp=quarantine; pct=100; rua=mailto:dmarc-reports@$ServerName; adkim=r; aspf=r; fo=1\"" ""
 
 # DMARC - domínio estável
-# create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:dmarc-reports@$ServerName; adkim=r; aspf=r; fo=1\"" ""
+create_or_update_record "_dmarc.$ServerName" "TXT" "\"v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:dmarc-reports@$ServerName; adkim=r; aspf=r; fo=1\"" ""
 
 # DKIM: precisa bater com o selector usado pelo Rspamd
 create_or_update_record "default._domainkey.$ServerName" "TXT" "\"v=DKIM1; h=sha256; k=rsa; p=$EscapedDKIMCode\"" ""
