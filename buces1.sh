3a574e8ac095587a1a05f2115b569513771329df
Flaviosxzxas <132931370+Flaviosxzxas@users.noreply.github.com>
2026-09-02 13:58:50 -0300

    Fix variable escaping in buces1.sh

--- buces1.sh
+++ buces1.sh
@@ -673,7 +673,7 @@ smtp_tls_note_starttls_offer = yes
 # Base
 mydomain = $ServerName
 myorigin = $ServerName
-mydestination = $myhostname, $mydomain, localhost.$mydomain, localhost
+mydestination = \$myhostname, \$mydomain, localhost.\$mydomain, localhost
 relayhost =
 mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
 smtpd_relay_restrictions = permit_mynetworks, reject_unauth_destination
