4e40bc066a12516042eb9e9096aea873d0c18d9e
connection <132931370+Flaviosxzxas@users.noreply.github.com>
2026-09-04 17:11:12 -0300

    Update mydestination configuration in buces2.sh
    Comment out the original mydestination line and set it to localhost.

--- buces2.sh
+++ buces2.sh
@@ -677,7 +677,8 @@ smtp_tls_note_starttls_offer = yes
 # Base
 mydomain = $ServerName
 myorigin = $ServerName
-mydestination = $MailServerName, localhost.$ServerName, localhost
+#mydestination = $MailServerName, localhost.$ServerName, localhost
+mydestination = localhost 
 relayhost =
 mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
 smtpd_relay_restrictions = permit_mynetworks, reject_unauth_destination
